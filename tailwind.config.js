600825930ee8c8f8642afa4e034f4a1dc527bead
Frederick Jones <adefredy1@gmail.com>
2023-10-14 03:35:48 +0100

    Commence Creation of Wreen Web App at 14/10/2023 03:34am using next13 with experimental server action enabled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  "mode": "jit",
+  "darkMode": "class",
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,11 +9,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        "grey": "#acada8",
+        "primary": "#09dba0",
+        "info": "#0979db",
+        "danger": "#ff6666",
+        "warning": "#fac451",
+      }
     },
   },
   plugins: [],
